1cfe43c2662c52c778fe17f17ff8f279232e1010
Shahrukh Akhtar <iam@shaz3e.com>
2025-04-14 18:05:38 +0500

    dynamic breadcrumb

---
+++ resources/views/components/page-title.blade.php
@@ -0,0 +1,24 @@
+<div class="page-header">
+    <div class="page-block">
+        <div class="row align-items-center">
+            <div class="col-md-12">
+                <ul class="breadcrumb">
+                    @foreach ($breadcrumbs as $breadcrumb)
+                        <li class="breadcrumb-item">
+                            @if (isset($breadcrumb['url']))
+                                <a href="{{ $breadcrumb['url'] }}">{{ $breadcrumb['label'] }}</a>
+                            @else
+                                {{ $breadcrumb['label'] }}
+                            @endif
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+            <div class="col-md-12">
+                <div class="page-header-title">
+                    <h2 class="mb-0">{{ $title }}</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
